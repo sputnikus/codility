f8aef43803b1bd27a77a871128af93758ab4fd50
Martin Putniorz <mputniorz@gmail.com>
2014-04-29 11:20:48 +0200

    GenomicRangeQuery final

--- GenomicRangeQuery.py
+++ GenomicRangeQuery.py
@@ -1,24 +1,27 @@
+IMPACTS = {'A':1, 'C':2, 'G':3, 'T':4,}
+
 def solution(S, P, Q):
-    impacts = {'A':1, 'C':2, 'G':3, 'T':4,}
     N = len(S)
     M = len(P)
-    factor_map = dict((key, [0]*N) for key in impacts)
+    factor_list = [[0]*N for _ in xrange(4)]
     
     for i, nucle in enumerate(S):
-        factor_map[nucle][i] += 1
+        factor_list[IMPACTS[nucle]-1][i] += 1
         if i < N - 1:
-            for factor in factor_map:
-                factor_map[factor][i+1] = factor_map[factor][i]
-        
+            for factor in factor_list:
+                factor[i+1] = factor[i]
+   
     for j in xrange(M):
+        lefty = P[j]
+        if lefty == Q[j]:
+            P[j] = IMPACTS[S[lefty]]
+            continue
         pre_sum = [0] * 4
-        if P[j] > 0:
-            pre_sum = [pre[P[j]] for pre in factor_map.values()]
-        post_sum = [post[Q[j]] for post in factor_map.values()]
+        if lefty > 0:
+            pre_sum = [pre[lefty-1] for pre in factor_list]
+        post_sum = [post[Q[j]] for post in factor_list]
         for k in xrange(4):
-            if post_sum[k] - pre_sum[k] > 0:
+            if post_sum[k] > pre_sum[k]:
                 P[j] = k + 1
                 break
-        else:
-            P[j] = impacts[S[P[j]]]
     return P
